c328e4af9935d5a569654a9bd034e2b00f481428
Timo S <58859419+timo-cmd@users.noreply.github.com>
2020-11-08 19:07:33 +0100

    Add some headers

---
+++ sources/worm.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#define __worm_h__
+#ifndef __worm_h__
+/* to be completed soon */
+#endif
+
+#define data-size 1000
+
+#define random rand()
+#define _BEAST_VERSION_NUMBER_ 0
+#define _BEAST_VERSION_MINOR_  0
+#define _BEAST_VERSION_PATCH_  1
+#define _BEAST_VERSION_EXTRA_ "-dev"
+#define _BEAST_VERSION "beast v0.0.1-dev"
+
+int main( void ) {
+  
+  return 0;
+}
