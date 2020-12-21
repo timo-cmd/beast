6ab730d2f55540fc3861d909ad596087828e8c00
Timo S <58859419+timo-cmd@users.noreply.github.com>
2020-12-21 15:05:45 +0100

    added miscellaneous stuff

---
+++ include/nemox.h
@@ -0,0 +1,34 @@
+#ifndef NEMOX_H
+#define NEMOX_H
+#endif
+
+#define SZ_STACK 100
+#define SZ_EHDR 564
+#define SZ_MMAP 24
+#define ELF 464c457fh
+#define PT_LOAD 1
+#define PT_DYNAMIC 2
+#define PT_PHDR 6
+#define TEXT_SECT 2
+#define DATA_SECT 3
+#define E_ENTRY 0x18
+#define SYS_READDIR 89
+#define SYS_READ 3
+#define SYS_WRITE 4
+#define SYS_OPEN 5
+#define SYS_CLOSE 6
+#define SYS_UNLINK 10 
+#define SYS_LSEEK 19
+#define SYS_MMAP 90
+#define SYS_MUNMAP 91
+#define SYS_FTRUNCATE 93
+#define SYS_FSTAT 108
+
+#define VERSION_MAJOR 0
+#define VERSION_MAJOR_MINOR 0
+#define VERSION_MINOR_MINOR 1
+#define VERSION_FULL 001
+#define VERSION_PROD "dev"
+
+#define AUTHOR "Timo Sarkar"
+#define MAIL "<sartimo10@gmail.com>"
