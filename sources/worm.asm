c7d00df0e700f0b24cc59990550e6491b038b2e4
Timo S <58859419+timo-cmd@users.noreply.github.com>
2020-12-21 16:19:47 +0100

    added constants

--- sources/worm.asm
+++ sources/worm.asm
@@ -18,5 +18,27 @@
 section .text
 global _start
 
+%define SZ_STACK 1000
+%define SZ_EHDR 564
+%define SZ_MMAP 24
+%define ELF 464c457fh
+%define PT_LOAD 1
+%define PT_DYNAMIC 2
+%define PT_PHDR 6
+%define TEXT_SECT 2
+%define DATA_SECT 3
+%define E_ENTRY 0x18
+%define SYS_READDIR 89
+%define SYS_READ 3
+%define SYS_WRITE 4
+%define SYS_OPEN 5
+%define SYS_CLOSE 6
+%define SYS_UNLINK 10 
+%define SYS_LSEEK 19
+%define SYS_MMAP 90
+%define SYS_MUNMAP 91
+%define SYS_FTRUNCATE 93
+%define SYS_FSTAT 108
+
 point db ".",0
 copyright db "Timo Sarkar MIT 2020"
